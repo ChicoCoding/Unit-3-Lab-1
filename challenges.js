b7c277ed00b4d3652556c09be01a8f96bb108548
Brendan Chico <boomaster40@gmail.com>
2022-10-03 18:08:02 -0400

    Extended Challenges Complete

--- challenges.js
+++ challenges.js
@@ -1,8 +1,22 @@
-let wage = 10;
-let hours = 20;
-
-if (hours <= 40) {
-  let pay = wage * hours;
-} else {
-  let pay = 40 * wage + (hours - 40) * (wage * 1.5);
+const wage = 10;
+const hours = 50;
+let weeks = 0;
+let pay = 0;
+let weekPay = 0;
+let preovertimePay = 40 * wage;
+let overtimeWage = wage * 1.5;
+let overtimeHours = hours - 40;
+while (pay < 1000000) {
+  if (hours <= 40) {
+    weekPay = wage * hours;
+    pay = pay + weekPay;
+    weeks++;
+  } else {
+    weekPay = preovertimePay + overtimeHours * overtimeWage;
+    pay = pay + weekPay;
+    weeks++;
+  }
+  console.log(`Week: ${weeks} Total Pay:${pay}`);
 }
+
+console.log(`It took ${weeks} weeks to earn $${pay} at $${weekPay} per week.`);
